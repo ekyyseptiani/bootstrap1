3697219da5bf19e7b0fcb4271fd4c63ed7c0eca6
ekyyseptiani <124320605+ekyyseptiani@users.noreply.github.com>
2023-02-13 11:29:35 +0700

    Add files via upload

---
+++ home_user.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+     integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" 
+     crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style3.css">
+
+    <title>Pendaftaran</title>
+  </head>
+  <body>
+
+   <!-- Navbar -->
+   <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" 
+        data-target="#navbarNavAltMarkup" 
+        -controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+           <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">
+                (current)</span></a>
+            <a class="nav-item nav-link" href="formulir.php">Formulir</a>
+            <a class="nav-item nav-link" href="informasi.php">Logout</a>
+        </div>
+        </div>
+        </div>
+  </nav>
+
+
+    <!--jumbotrom-->
+    <div class="">
+        <div class="container">
+          <center><h1 class="display-4">anda login sebagai user</h1></ center>
+          <p class="lead">You can register by clicking the form button above on the right, you can choose if you want to return to the login form you can click <br>
+          the logout button, if you want to return to the information form you can click the home button.</p>
+        </div>
+      </div>
+    <!--akhir jumbotrom-->
+
+  
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+     crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+     integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" 
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" 
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" 
+    integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+"
+     crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
